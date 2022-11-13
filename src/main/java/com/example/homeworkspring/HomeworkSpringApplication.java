01dbd768b9a62401c6c7b9fa6a07ad9bc71d0400
zemscky <aleksei.zemscky@yandex.ru>
2022-11-13 15:52:49 +0300

    Initial commit

---
+++ src/main/java/com/example/homeworkspring/HomeworkSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkspring/HomeworkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
